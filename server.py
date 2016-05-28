0d2206fe1c7c81e24a028af3cce945c97e5c3da0
Steve Purves <stevejpurves@gmail.com>
2016-05-28 12:08:32 +0200

    init

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, send_from_directory
+
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html');
+
+@app.route('/<path:path>')
+def static_proxy(path):
+  # send_static_file will guess the correct MIME type
+  return app.send_static_file(path)
+    
+if __name__ == "__main__":
+    app.run()
