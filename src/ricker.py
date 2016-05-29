7be90a0622030fd76723f13291c3294dd499977f
Henrique Santos <hbuenos@gmail.com>
2016-05-29 11:24:23 +0200

    Just space

--- src/ricker.py
+++ src/ricker.py
@@ -5,24 +5,24 @@
 import pylab as np
 from signalanalysis import Rotation
 
-def Ricker(ns, dt,fp, phase=0.0, A=1.0):
-    
+def Ricker(ns, dt, fp, phase=0.0, A=1.0):
+
     '''
     Function Ricker: create Ricker wavelet
-    
+
     Input:
     ns - number os samples (scalar)
     dt - sampling interval (scalar, seconds)
     fp - peak frequency (scalar, Hz)
     phase - angle (scalar, unit: degree)
     A  - amplitude scaling factor (scalar)
-    
+
     Output:
     pulse: ricker wavelet (array)
     '''
     phase = np.rad2deg(phase)
     delay = (ns-1)/2
-  
+
     t = np.arange(0,ns,1)*dt - delay*dt
     pulse=A*(1.-2.*(np.pi*fp*t)**2.)*np.exp(-1.*(np.pi*fp*t)**2.)
     pulse = Rotation(pulse, phase)
